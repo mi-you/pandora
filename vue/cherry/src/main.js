e4812711f82d9730cba37eedce2eff3b5d9ca064
娄双占 <15755276427@163.com>
2020-12-01 09:07:45 +0800

    迭代

--- vue/cherry/src/main.js
+++ vue/cherry/src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import utils from './utils'
 
+import 'normalize.css'
+import 'style/index.css'
+
+Vue.use(utils)
 Vue.config.productionTip = false
 
 new Vue({
--- vue/cherry/src/router/index.js
+++ vue/cherry/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from 'views/home/Index.vue'
+const VideoPlay = () => import(/* webpackChunkName: "video" */ 'views/videoPlay/Index.vue')
+const AudioPlay = () => import(/* webpackChunkName: "audio" */ 'views/audioPlay/Index.vue')
+const PdfToImage = () => import(/* webpackChunkName: "pdf" */ 'views/pdfToImage/Index.vue')
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/videoPlay',
+    name: 'videoPlay',
+    component: VideoPlay
+  },
+  {
+    path: '/audioPlay',
+    name: 'audioPlay',
+    component: AudioPlay
+  },
+  {
+    path: '/pdfToImage',
+    name: 'pdfToImage',
+    component: PdfToImage
   }
 ]
 
